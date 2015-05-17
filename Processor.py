f8460e64b661282f83877670f79d337cab191322
Ben Magee <bmage@cogative.com>
2015-05-17 18:21:39 +0100

    Added percentage chance configuration.

--- Processor.py
+++ Processor.py
@@ -5,12 +5,17 @@ import random
 
 class RedditCommentProcessor:
     target_phrases = []  # Phrases to look for in comments
+    percentage_chance = None  # Chance of posting a comment
 
     def __init__(self):
         target_phrases_file = open('config/target_phrases.csv', 'r')
         target_phrases_content = target_phrases_file.read()
         self.target_phrases = target_phrases_content.split(',')
         target_phrases_file.close()
+
+        percentage_chance_file = open('config/percentage_chance.txt', 'r')
+        self.percentage_chance = percentage_chance_file.read()
+        percentage_chance_file.close()
 
     def process_comments(self, comments):
         """
@@ -33,7 +38,7 @@ class RedditCommentProcessor:
                     if phrase.lower() is not "catnip" or phrase.lower() is not "i would like to subscribe to cat facts":
                         # Play a chance game as to whether to post a comment!
                         # Currently a 20% chance of posting a comment
-                        if random.randrange(0, 100) < 20:
+                        if random.randrange(0, 100) < int(self.percentage_chance):
                             matched_comments.append(comment)
                     else:
                         # If the user replied with catnip we know they want a response!
